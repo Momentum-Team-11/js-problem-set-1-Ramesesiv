57a44456a04ed6babedcd2a468fe49b29894b869
Ramesesiv <x.j.sutherland@gmail.com>
2022-02-01 11:33:17 -0600

    message

--- Desktop/Momentum/js-problem-set-1-Ramesesiv-main/homework.js
+++ Desktop/Momentum/js-problem-set-1-Ramesesiv-main/homework.js
@@ -1,38 +1,91 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+let givenName= "Addison"
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-
+ let candies = 20
+ let people = 6
+ let leftover= candies % people
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+function greeting(name) {
+   return name= "hello,"+name
 
+}
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(p1){
+p1= prompt("Enter a number")
+if(p1%2==1 || p1%2==-1){
+    return true
+}
+else 
+    return false
+}
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-
+function isOdd(p2){
+    p1= prompt("Enter a number")
+    if(p2%2==0){
+        return true
+    }
+    else 
+        return false
+    }
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
-
+function farenheitToCelsius(celsius){
+ var toFahr= celsius*9/5+32
+console.log(toFahr)
+}
 // 7. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(Fahrenheit){
+    var fTemp= Fahrenheit
+    var ftoCel= (fTemp-32)*5/9
+    var message= ftemp
+    console.log(message)
+}
+
+
+
 
 // 8. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Kelvin. This function must use your previous
-// fahrenheitToCelsius function.
+// fahrenheitT oCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin(fTemp){
+    var ktofTemp
+celsiusToFahrenheit(ktofTemp)
+if(ktofTemp= -273.15){
+console.log("Tempature is Absolute Zero (0K)")
 
+}
+else{
+    console.log(ktofTemp)
+}
+
+}
 // 9. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser(num1,num2){
+if (num1< num2){
+return num1
 
+}
+else if (num2<num1){
+
+    return num2
+}
+
+}
 // 10. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
 // in the specified language. The supported languages and their
@@ -71,3 +124,19 @@
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+//Write a JS function called double that takes a number and returns two times that number
+
+
+
+
+
+
+
+
+function Doubledouble(x){
+var doubleD= x
+ doubleD*2
+ return console.log(doubleD)
+
+}
+Doubledouble(7)
